efc7279e260f222230c0b67c2a49b3b14f79bb0a
arcraith <arcraith@live.com>
2022-07-02 01:02:56 -0700

    Upgrade to core 3.1

---
+++ DemoApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Icepack;
+
+namespace DemoApp
+{
+    internal class Program
+    {
+        [SerializableObject]
+        private class DataClass
+        {
+            public int Field1 { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                Serializer serializer = new Serializer();
+                serializer.Serialize(GetData(), stream);
+                stream.Position = 0;
+                serializer.Deserialize<List<DataClass>>(stream);
+            }
+        }
+
+        private static object GetData()
+        {
+            List<DataClass> data = new List<DataClass>();
+            for (int i = 0; i < 10000000; i++)
+            {
+                DataClass item = new DataClass();
+                item.Field1 = 123;
+
+                data.Add(item);
+            }
+
+            return data;
+        }
+    }
+}
