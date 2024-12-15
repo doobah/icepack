9340b65322ca1fe529d8e1b2a6edbdf5ff15da0a
arcraith <arcraith@live.com>
2024-12-15 18:25:37 +1100

    Bump version

--- Icepack/Serializer.cs
+++ Icepack/Serializer.cs
@@ -13,7 +13,7 @@ namespace Icepack;
 public sealed class Serializer
 {
     /// <summary> Serializers with the same compatibility version are guaranteed to be interoperable. </summary>
-    public const ushort CompatibilityVersion = 6;
+    public const ushort CompatibilityVersion = 7;
 
     /// <summary> Keeps track of type information. </summary>
     private readonly TypeRegistry typeRegistry;
